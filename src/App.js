4a0aee2e9bb943ae1a1a23fc11ad6991c76dc0f4
bigv12 <duruewuruikenna12@gmail.com>
2023-02-12 18:23:28 +0100

    first commit of the react to do list app

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import styles from './app.module.css';
+
+
+function App() {
+
+const [textInput, setInput]=useState('')
+let [listItem, setListItem]= useState([])
+
+
+
+  
+  const add = () => {
+    setListItem(
+      [
+        ...listItem,
+        textInput,
+
+      
+      ]
+    )  
+  }
+  
+  const del = (newww) => {
+    const newList = listItem.filter((textInput) => textInput !== newww)
+    setListItem(newList)
+  }
+
+
+  console.log(textInput)
+
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.container}>
+        <h2>To Do List App</h2>
+      <input value={textInput} onChange={ (e)=> setInput(e.target.value)}  />
+
+        <button onClick={add} className={styles.submit}> add item </button>
+        <h2>List Items</h2>
+      <ul>
+				{listItem.map(listItem => (
+          <li key={Math.floor(Math.random() * 100)}>{listItem} <button  className={styles.dele} onClick={()=>del(listItem)}>delte item</button></li>
+          
+				))}
+        </ul>
+        </div>
+    </div>
+  );
+}
+
+export default App;
